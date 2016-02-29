16d4387567590cadb5454969f803245b19306172
Zeno Go <lambda.guo@gmail.com>
2016-02-29 23:59:44 +0800

    init keywords extract

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -3,6 +3,7 @@
 import unittest
 import re
 from moebot.wikipedia_client import client
+from moebot.keywords_algorithm import extract_text
 
 
 class WikipediaClientTest(unittest.TestCase):
@@ -10,3 +11,11 @@ class WikipediaClientTest(unittest.TestCase):
     def runTest(self):
         response = client("https://zh.wikipedia.org/wiki/Wikipedia:%E9%A6%96%E9%A1%B5")
         assert re.match(r"^[\s\S]*Gotzehsing[\s\S]*$", response.read())
+
+
+class HTMLAnalyseTest(unittest.TestCase):
+
+    def runTest(self):
+        with open("tests/test.html") as f:
+            result = extract_text(f.read())
+            assert result != []
